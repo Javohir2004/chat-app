22488e4da84b93bbfc3fc5c41697446b88d38028
Javohir2004 <javohirbobojonov21@gmail.com>
2024-02-18 17:38:54 +0500

    wow its a chat app!

--- app/script.ts
+++ app/script.ts
@@ -1,6 +1,7 @@
 import { Types } from "ably/promises";
 import * as Ably from "ably/promises";
 
+
 (async () => {
     const optionalClientId = "optionalClientId"; // When not provided in authUrl, a default will be used.
     const ably = new Ably.Realtime.Promise({ authUrl: `/api/ably-token-request?clientId=${optionalClientId}` });
@@ -10,7 +11,7 @@ import * as Ably from "ably/promises";
     const form = document.getElementById("form");
     const input = document.getElementById("input") as HTMLInputElement;
     const chat_id = localStorage.getItem("chat_id");
-
+    const showuser = document.getElementById("show-online-users-btn");
     form.addEventListener("submit", (e:SubmitEvent) => {
         e.preventDefault();
 
@@ -32,17 +33,43 @@ import * as Ably from "ably/promises";
 
     });
     localStorage.clear();
-
+    let spanValues = [];
     await channel.subscribe((msg: Types.Message) => {
-        const messageElement = document.createElement("div");
-        messageElement.classList.add("message");
+        const messageElement = document.createElement("span");
+        messageElement.classList.add("msg");
         messageElement.innerHTML = msg.data;
 
         const author = msg.connectionId === ably.connection.id ? "me" : "other";
-        messageElement.setAttribute("data-author", author);
+        messageElement.setAttribute("id", author);
 
         messages.appendChild(messageElement);
-    });
+
+        const spanElements = document.querySelectorAll("span.msg");
+        spanValues = [];
+        spanElements.forEach((span) => {
+            spanValues.push(span.textContent);
+        });
+        });
+        let values = [];
+        showuser.addEventListener("click", () => {
+            let str: string = "";
+            spanValues.forEach((item) => {
+                let sub: string = item;
+                values.push(sub.substring(0, sub.indexOf(":")));
+            });
+            let uniquevalues = [...new Set(values)];
+            uniquevalues.forEach((item) => {
+                str = str + item + ",";
+            });
+            if (str.trim() == "") 
+            {
+                alert("No one online");
+            }
+            else
+            {
+            alert("Online users: " + str.substring(0, str.length-1));
+            }
+        });
 })();
 
 export { };
